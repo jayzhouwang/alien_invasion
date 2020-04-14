c2e4ab89208288c4132deaa94283cf80d8244af8
jackwang <jayzhouwang@gmail.com>
2020-04-15 01:02:28 +0800

    level up

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ class Settings:
     """A class to store all settings for Alien Ivasion"""
 
     def __init__(self):
-        """Initialize the game's settings."""
+        """Initialize the game's static settings."""
         # screen settings
         self.screen_width = 1000
         self.screen_height = 600
@@ -17,10 +17,30 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
-        self.bullets_allowed = 3
+        self.bullets_allowed = 5
 
         # Alien settings
         self.alien_speed = 1.0
         self.fleet_drop_speed = 10
         # fleer_direction of 1 represent right; -1 represent left.
         self.fleet_direction = 1
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+        
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change thoughout the game."""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
+        # fleer_direction of 1 represent right; -1 represent left.
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Increase speed settings."""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
